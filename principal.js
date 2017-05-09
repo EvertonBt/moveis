7937e371a73614bcf63da573f830abbb33460516
Everton B <Everton B>
2017-05-08 21:30:02 -0300

    Alterando efeito de banner

--- principal.js
+++ principal.js
@@ -1,10 +1,12 @@
 var banners =
-["Os melhores do Brasil!", "Qualidade e preço baixo!"];
+["Os melhores do Brasil!", "Qualidade e preço baixo!", "Venha já"];
 var bannerAtual = 0;
 
 function trocaBanner() {
-bannerAtual = (bannerAtual + 1) % 2;
-document.querySelector('h2#mensagem').textContent =
-banners[bannerAtual];
+document.querySelector('h2#mensagem').textContent = banners[bannerAtual];
+bannerAtual = bannerAtual + 1;
+if(bannerAtual >= 3){
+	bannerAtual = 0;
+}
 }
 setInterval(trocaBanner, 2000);
